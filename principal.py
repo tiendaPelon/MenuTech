e76441f64ded2ffa7cb923bb7279ad9b487ee5b2
Yasmin <yasminrodriguezc@micorreo.upp.edu.mx>
2023-12-03 17:45:45 -0600

    Se agrego la función de actualizar_completado

--- principal.py
+++ principal.py
@@ -1,5 +1,6 @@
 import threading
 from pymongo import MongoClient
+from bson import ObjectId
 
 def escribir_base_de_datos(hilo_id, num_comanda, barbacoa, quesadilla, bebida, consome):
     # Realizar la conexión a la base de datos
@@ -9,12 +10,12 @@ def escribir_base_de_datos(hilo_id, num_comanda, barbacoa, quesadilla, bebida, c
 
     # Insertar el valor en la base de datos
     comanda = {
-    "num_comanda": num_comanda,
-    "barbacoa": barbacoa,
-    "quesadilla": quesadilla,
-    "bebida": bebida,
-    "consome": consome,
-    "completado": False  
+        "num_comanda": num_comanda,
+        "barbacoa": barbacoa,
+        "quesadilla": quesadilla,
+        "bebida": bebida,
+        "consome": consome,
+        "completado": False  
     }
     result = comandas_collection.insert_one(comanda)
     print(f"Hilo {hilo_id}: Comanda insertada con ID: {result.inserted_id}")
@@ -38,10 +39,19 @@ def mostrar_datos():
         print("Quesadilla:", comanda["quesadilla"])
         print("Bebida:", comanda["bebida"])
         print("Consomé:", comanda["consome"])
+
+        # Verificar si 'completado' está presente 
+        completado = comanda.get("completado")
+        if completado is not None:
+            print("Completado:", completado)
+        else:
+            print("Completado: N/A")
+
         print("")
 
     # Cerrar la conexión
     client.close()
+
 
 def borrar_datos():
     # Realizar la conexión a la base de datos
@@ -56,9 +66,41 @@ def borrar_datos():
     comandas_collection.delete_one(bus)
     print("Registro borrado")
 
+    # Cerrar la conexión
+    client.close()
+
+def actualizar_completado():
+    # Realizar la conexión a la base de datos
+    client = MongoClient("mongodb+srv://danielxp:maspormasDF1@cluster0.glu8e4r.mongodb.net/?retryWrites=true&w=majority")
+    db = client["danielxp88"]
+    comandas_collection = db["comandas"]
+
+    # Mostrar las comandas y solicitar el ID de la comanda a actualizar
+    print("-----------------")
+    print("Comandas en la base de datos:")
+    for comanda in comandas_collection.find():
+        print("ID:", comanda["_id"])
+        print("Número de Comanda:", comanda["num_comanda"])
+
+        # Verificar si se encuentra 'completado' 
+        completado = comanda.get("completado")
+        if completado is not None:
+            print("Completado:", completado)
+        else:
+            print("Completado: N/A")
+
+        print("")
+
+    comanda_id = input("Ingrese el ID de la comanda que desea actualizar: ")
+    completado = input("¿La comanda está completada? (Sí/No): ").lower() == "sí"
+
+    # Actualizar el campo "completado" de la comanda seleccionada
+    comandas_collection.update_one({"_id": ObjectId(comanda_id)}, {"$set": {"completado": completado}})
+    print("Se ha actualizado")
 
     # Cerrar la conexión
     client.close()
+
 
 def menu():
     while True:
@@ -66,7 +108,8 @@ def menu():
         print("1. Agregar Comanda")
         print("2. Borrar Comanda")
         print("3. Consultar Comandas")
-        print("4. Salir")
+        print("4. Actualizar Estado de Comanda")
+        print("5. Salir")
 
         opcion = input("Seleccione una opción: ")
 
@@ -84,6 +127,10 @@ def menu():
             hilo.start()
             hilo.join()
         elif opcion == "4":
+            hilo = threading.Thread(target=actualizar_completado, name=f'HiloActualizarComanda')
+            hilo.start()
+            hilo.join()
+        elif opcion == "5":
             print("¡Gracias por utilizarlo, adiós:)!")
             break
         else:
@@ -112,6 +159,3 @@ def obtener_opcion(opcion):
 
 if __name__ == "__main__":
     menu()
-    
-
-
